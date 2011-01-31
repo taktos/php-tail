3248a6085b1848970a831e136261678b4b3623b6
Peeter.Tomberg@gmail.com <Peeter.Tomberg@gmail.com@2dfc9bd2-8546-ee59-d7b2-ba1846a80a06>
2011-01-31 13:40:16 +0000

    Initial project import. Probably the only one unless bugs are reported.
    git-svn-id: http://php-tail.googlecode.com/svn/trunk@3 2dfc9bd2-8546-ee59-d7b2-ba1846a80a06

---
+++ Log.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Require the library
+ */
+require 'PHPTail.php';
+/**
+ * Initilize a new instance of PHPTail
+ * @var PHPTail
+ */
+$tail = new PHPTail("C:/Program Files/Apache Software Foundation/Apache2.2/logs/access.log");
+
+/**
+ * We're getting an AJAX call
+ */
+if(isset($_GET['ajax']))  {
+	echo $tail->getNewLines($_GET['lastsize'], $_GET['grep'], $_GET['invert']);
+	die();
+}
+/**
+ * Regular GET/POST call, print out the GUI
+ */
+$tail->generateGUI();
